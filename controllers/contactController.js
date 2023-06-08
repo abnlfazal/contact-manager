b3a317296b0c3ac1274fa37abb3ea33f9f0c31d4
Abul Fazal <mailsfazal@gmail.com>
2023-06-08 17:44:26 +0530

    added crud operations for all apis

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -20,7 +20,13 @@ const createContact = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("All fields are mandatory");
     }
-    res.status(201).json({message: "Create a contact"});
+
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    });
+    res.status(201).json(contact);
 });
 
 //@desc Get contact
@@ -28,7 +34,12 @@ const createContact = asyncHandler(async (req, res) => {
 //@access public
 
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Get contact for id ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 //@desc Update contact
@@ -36,15 +47,28 @@ const getContact = asyncHandler(async (req, res) => {
 //@access public
 
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update contact for id ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete contact for id ${req.params.id}`});
+const deleteContact = asyncHandler(async (req, res) => { 
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    await Contact.findByIdAndDelete(req.params.id);
+    res.status(200).json(contact);
 });
 
 
